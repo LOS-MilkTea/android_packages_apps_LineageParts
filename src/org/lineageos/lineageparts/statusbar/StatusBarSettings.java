c8aa64c4246c7ccc552606b5439bd34bb99184e9
Dan Pasanen <dan.pasanen@gmail.com>
2019-11-04 23:17:25 -0600

    [2/3] LineageParts: Custom quick settings tile columns
    Change-Id: Ied14d0ba5d3918142bfd5950155acb0608c89e7f

--- src/org/lineageos/lineageparts/statusbar/StatusBarSettings.java
+++ src/org/lineageos/lineageparts/statusbar/StatusBarSettings.java
@@ -52,6 +52,7 @@ public class StatusBarSettings extends SettingsPreferenceFragment
     private static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
     private static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
     private static final String STATUS_BAR_QUICK_QS_PULLDOWN = "qs_quick_pulldown";
+    private static final String STATUS_BAR_QS_TILE_COLUMNS = "status_bar_qs_tile_columns";
 
     private static final int STATUS_BAR_BATTERY_STYLE_TEXT = 2;
 
@@ -62,6 +63,7 @@ public class StatusBarSettings extends SettingsPreferenceFragment
     private static final String NETWORK_TRAFFIC_SETTINGS = "network_traffic_settings";
 
     private LineageSystemSettingListPreference mQuickPulldown;
+    private LineageSystemSettingListPreference mQSTileColumns;
     private LineageSystemSettingListPreference mStatusBarClock;
     private LineageSystemSettingListPreference mStatusBarAmPm;
     private LineageSystemSettingListPreference mStatusBarBattery;
@@ -101,6 +103,10 @@ public class StatusBarSettings extends SettingsPreferenceFragment
         mQuickPulldown = findPreference(STATUS_BAR_QUICK_QS_PULLDOWN);
         mQuickPulldown.setOnPreferenceChangeListener(this);
         updateQuickPulldownSummary(mQuickPulldown.getIntValue(0));
+
+        mQSTileColumns =
+                (LineageSystemSettingListPreference) findPreference(STATUS_BAR_QS_TILE_COLUMNS);
+        mQSTileColumns.setOnPreferenceChangeListener(this);
     }
 
     @Override
